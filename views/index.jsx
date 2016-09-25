e25db2929cc552fdf6520fcf1dba7a3adc5b02d8
yuya okagawa <yokagawa.dev@gmail.com>
2016-09-25 22:07:05 +0900

    ボタン追加

--- views/index.jsx
+++ views/index.jsx
@@ -10,6 +10,17 @@ export default class SalaryInputTable extends React.Component {
             <div className="content">
               <h2 class="title is-2">基準雇用者給与等支給額の入力</h2>
               <div id="salary_input_table" >{this.props.SalaryInputTable}</div>
+              <div className="control is-horizontal is-pulled-right">
+              <div className="control is-grouped">
+                <span className="control">
+                <button className="button is-info is-medium">保存</button>
+                </span>
+                <span className="control">
+                <button className="button is-light is-medium">取消</button>
+                </span>
+                </div>
+              </div>
+              <div className="is-clearfix"></div>
             </div>
           </div>
         </section>
